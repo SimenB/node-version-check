4fe85bbeb77d4f1058561b7dae52ece9b75d1a6a
Simen Bekkhus <sbekkhus91@gmail.com>
2016-07-08 18:32:56 +0200

    Add linting

--- index.js
+++ index.js
@@ -2,19 +2,16 @@
 
 var semver = require('semver');
 
-function makeSemverString (version) {
+function makeSemverString(version) {
   if (typeof version === 'number') return version + '.0.0';
 
   return version;
 }
 
 module.exports.gte = function nodeVersionCheck(version) {
-  version = makeSemverString(version);
-
-  return semver.gte(process.version, version);
+  return semver.gte(process.version, makeSemverString(version));
 };
-module.exports.lt = function nodeVersionCheck(version) {
-  version = makeSemverString(version);
 
-  return semver.lt(process.version, version);
+module.exports.lt = function nodeVersionCheck(version) {
+  return semver.lt(process.version, makeSemverString(version));
 };
