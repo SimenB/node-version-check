4ccb3f6b11fd338070cd674b6d67bc6cb03bb0a4
Simen Bekkhus <sbekkhus91@gmail.com>
2016-07-29 13:26:58 +0200

    Add some tests

---
+++ test/test.js
@@ -0,0 +1,35 @@
+import test from 'ava';
+
+import { satisfies, gte, lt } from '../index';
+
+let originalVersion;
+
+test.before(() => {
+  originalVersion = process.version;
+  Object.defineProperty(process, 'version', {
+    value: 'v5.0.0',
+  });
+});
+
+test.after.always(() => {
+  Object.defineProperty(process, 'version', {
+    value: originalVersion,
+  });
+});
+
+test('satisfies', t => {
+  t.true(satisfies('<6'));
+  t.false(satisfies('<5'));
+  t.false(satisfies('>5'));
+  t.true(satisfies('=5'));
+});
+
+test('gte', t => {
+  t.false(gte(6));
+  t.true(gte(5));
+});
+
+test('lt', t => {
+  t.true(lt(6));
+  t.false(lt(5));
+});
